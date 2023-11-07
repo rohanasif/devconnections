128b3ca9d684afeaa9fd431dcd7dad445461dc04
rohanasif1990 <rohanasif1990@gmail.com>
2023-11-07 14:30:07 +0500

    create redux boilerplate

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import store from "./store/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,17 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "../reducers";
+import thunk from "redux-thunk";
+
+const reducer = combineReducers({
+  main: rootReducer,
+});
+
+const middleware = [thunk];
 
 const store = createStore(
-  rootReducer,
+  reducer,
   composeWithDevTools(applyMiddleware(...middleware))
 );
+
+export default store;
